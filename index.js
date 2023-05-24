57e1a8ac7f85763beca00556346a14c580ced5d0
Justin Rodriguez <justin.r@me.com>
2023-05-24 12:07:44 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function isWin(element){
+    return (element.result === 'W')
+}
+
+function superbowlWin(record){
+    if (record.find(isWin)){
+        return record.find(isWin).year
+    }
+    else return record.find(isWin)
+
+}
